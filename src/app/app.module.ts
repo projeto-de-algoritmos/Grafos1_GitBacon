0a7eaf12755907396f61d310b03b425e4df714e0
Nicolas <nszchagas@gmail.com>
2023-04-22 21:53:19 -0300

    (WIP) Tela inicial.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatIconModule} from "@angular/material/icon";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {ComponentsModule} from "./components/components.module";
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatCardModule,
+        MatExpansionModule,
+        MatIconModule,
+        MatTooltipModule,
+        MatInputModule,
+        MatButtonModule,
+        ComponentsModule,
+        ReactiveFormsModule,
+        HttpClientModule
+
+    ],
+    providers: [HttpClient],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
