dccc6f9aa51b3655a7f191c32f844bf6e57aa102
Nicolas <nszchagas@gmail.com>
2023-05-01 15:25:45 -0300

    Ajustes no layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatCardModule} from "@angular/material/card";
-import {MatExpansionModule} from "@angular/material/expansion";
-import {MatIconModule} from "@angular/material/icon";
-import {MatTooltipModule} from "@angular/material/tooltip";
-import {MatInputModule} from "@angular/material/input";
-import {MatButtonModule} from "@angular/material/button";
-import {ComponentsModule} from "./components/components.module";
-import {ReactiveFormsModule} from "@angular/forms";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
-import {NgOptimizedImage} from "@angular/common";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatSelectModule} from "@angular/material/select";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from "@angular/material/card";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { ComponentsModule } from "./components/components.module";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { NgOptimizedImage } from "@angular/common";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSelectModule } from "@angular/material/select";
 
 @NgModule({
     declarations: [
@@ -35,11 +35,12 @@ import {MatSelectModule} from "@angular/material/select";
         NgOptimizedImage,
         MatToolbarModule,
         MatSidenavModule,
-        MatSelectModule
+        MatSelectModule,
+        MatSidenavModule
 
     ],
     providers: [HttpClient],
     bootstrap: [AppComponent]
 })
-export class AppModule {
+    export class AppModule {
 }
--- src/app/components/info-component/info-component.component.ts
+++ src/app/components/info-component/info-component.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class InfoComponentComponent {
 
+  public showRef = false;
+
 }
